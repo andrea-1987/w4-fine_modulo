0440beefefbd07dd5da91733e002dfe809011dd8
andrea-1987 <142450665+andrea-1987@users.noreply.github.com>
2024-01-26 00:27:38 +0100

    createNewCard

--- array.js
+++ array.js
@@ -1,9 +1,9 @@
-export const animeList =[
+export let animeList =[
     {
       name: "Naruto",
       description: "Storia di un giovane ninja con il sogno di diventare il leader del suo villaggio.",
       brand: "Pierrot",
-      imageUrl: "https://www.tvovermind.com/wp-content/uploads/2018/11/Naruto-Shipp%C5%ABden.jpg",
+      imageUrl: "https://th.bing.com/th/id/OIP.bGdWS3E5-YA-uBPwaufWAwHaEo?rs=1&pid=ImgDetMain",
       episodes: 220,
       price: 30,
       userId:"admin"
@@ -15,7 +15,7 @@ export const animeList =[
       imageUrl: "https://i.pinimg.com/originals/b6/e6/73/b6e6739b5435897b4d597381758927e8.jpg",
       episodes: 75,
       price: 25,
-      userId:"admin"
+      userId:"admin1"
     },
     {
       name: "My Hero Academia",
@@ -24,7 +24,7 @@ export const animeList =[
       imageUrl: "https://vignette.wikia.nocookie.net/dubbing9585/images/d/d2/My_Hero_Academia_Heroes_Rising.jpg/revision/latest?cb=20200419060653",
       episodes: 104,
       price: 18,
-      userId:"admin"
+      userId:"admin2"
     }
   ];
   
--- index.js
+++ index.js
@@ -1,50 +1,107 @@
 import { animeList } from "./array.js";
+let ARRAY = animeList;
 
- 
-  const url = "https://striveschool-api.herokuapp.com/api/product/";
-  const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIxMTk1NDkxM2Y2NTAwMThkMDkwNjYiLCJpYXQiOjE3MDYxMzQ4ODIsImV4cCI6MTcwNzM0NDQ4Mn0.redpAHUHV4GI4Te7YlGTT5d8ji3IZowqSqN59NwwDkg"
-  const cardFind = document.getElementById("cardFind");
-  
-  async function fetchPosts() {
-    try {
-      const resp = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${key}`,
-        },
-        body: JSON.stringify(animeList),
-      });
-      if (!resp.ok) {
-        throw new Error(`Errore HTTP! Status: ${resp.status}`);
-      }
-  
-      const data = await resp.json();
+const form = document.getElementById("form");
+const elimina = document.querySelector(".delete");
+const url = "https://striveschool-api.herokuapp.com/api/product/";
+const key =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWIxMTk1NDkxM2Y2NTAwMThkMDkwNjYiLCJpYXQiOjE3MDYyMjAxODMsImV4cCI6MTcwNzQyOTc4M30.zymE4u6MC23Yca3-2L7lOTh1GWWHpW9LM_0HhWEvm5Y";
+const cardFind = document.getElementById("cardFind");
 
-      cardFind.innerHTML = "";
-      for (const dati of data) {
-        cardFind.innerHTML += `
-        <div class="card col-3 mx-3 my-3" style="width: 18rem;">
-          <img src="${dati.imageUrl}" class="card-img-top col-3" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${dati.name}</h5>
-            <p class="card-text">${dati.description}</p>
-            <p class="card-text">$ ${dati.price}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>`;
-      }
-    } catch (error) {
-      console.log(error);
+async function fetchPosts() {
+  try {
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+      body: JSON.stringify(ARRAY),
+    });
+    if (!resp.ok) {
+      throw new Error(`Errore HTTP! Status: ${resp.status}`);
     }
-  }  fetchPosts(); // Chiamata POST
-  
+    
+    const data = await resp.json();
+    
+    cardFind.innerHTML = "";
+    for (const dati of data) {
+      cardFind.innerHTML += `
+      <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+      <img src="${dati.imageUrl}" class="card-img-top col-3" alt="...">
+      <div class="card-body">
+      <h5 class="card-title">${dati.name}</h5>
+      <p class="card-text">${dati.description}</p>
+      <p class="card-text">$ ${dati.price}</p>
+      <a href="#" class="btn btn-primary" class="dettagli">Mostra Dettagli</a>
+      <a href="#" class="btn btn-primary" class="delete">Delete</a>
+      </div>`;
+    }
+  } catch (error) {
+    console.log("postError",error);
+  }
+}
+fetchPosts(); // Chiamata POST
+
+
+async function createCard() {
+  const urlImg = document.getElementById("urlImg").value;
+  const product = document.getElementById("product").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+
+  const newCard = {
+    name: product,
+    description: description,
+    brand: "Pierrot",
+    imageUrl: urlImg,
+    price: price,
+    userId: "",
+  };
+
+  try {
+    const resp = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(newCard),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+    });
+
+    if (!resp.ok) {
+      throw new Error(`Errore HTTP! Status: ${resp.status}`);
+    }
+
+    const newCardData = await resp.json();
+    cardFind.innerHTML+= `
+      <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+        <img src="${newCardData.imageUrl}" class="card-img-top col-3" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${newCardData.name}</h5>
+          <p class="card-text">${newCardData.description}</p>
+          <p class="card-text">$ ${newCardData.price}</p>
+          <a href="#" class="btn btn-primary" class="dettagli">Mostra Dettagli</a>
+          <a href="#" class="btn btn-primary" class="delete">Delete</a>
+        </div>
+      </div>`;
+  } catch (error) {
+    console.log("create", error);
+  }
+}
+
+
+  form.addEventListener("submit",  event=>{
+    event.preventDefault();
+    createCard()});
+
   async function fetchGet() {
     try {
       const response = await fetch(url, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${key}`,
+          Authorization: `Bearer ${key}`,
         },
       });
   
@@ -52,26 +109,45 @@ import { animeList } from "./array.js";
         throw new Error(`Errore HTTP! Status: ${response.status}`);
       }
   
-      const data = await response.json();
-      // Pulisci il contenuto di cardFind prima di aggiungere nuovi elementi
-    //   cardFind.innerHTML = "";
-      
-        // for (const dati of data) {
-        //   cardFind.innerHTML += `
-        //     <img src="${dati.imageUrl}" class="card-img-top" alt="...">
-        //     <div class="card-body">
-        //       <h5 class="card-title">${dati.name}</h5>
-        //       <p class="card-text">${dati.description}</p>
-        //       <p class="card-text">${dati.price}</p>
-        //       <a href="#" class="btn btn-primary">Go somewhere</a>
-        //     </div>`;
-        // }
-     
+      // const data = await response.json();
+      // for (let dati of data) {
+      //   cardFind.innerHTML += `
+      //     <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+      //     <img src="${dati.imageUrl}" class="card-img-top col-3" alt="...">
+      //     <div class="card-body">
+      //     <h5 class="card-title">${dati.name}</h5>
+      //     <p class="card-text">${dati.description}</p>
+      //     <p class="card-text">$ ${dati.price}</p>
+      //     <a href="#" class="btn btn-primary" id="dettagli">Mostra Dettagli</a>
+      //     <a href="#" class="btn btn-primary" id="delete+${dati._id}">Delete</a>
+      //     </div>`;
+      // }
     } catch (error) {
-      console.log(error);
+console.log("aiuto", error);
     }
-  }
+  };fetchGet() // Chiamata GET
   
 
-  fetchGet();   // Chiamata GET
-  
+const deleteCard=async()=>{
+  try{
+  const response = await fetch(
+    `https://striveschool-api.herokuapp.com/api/product/${data._id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  console.log("User deleted successfully");
+} catch (error) {
+  console.error("delete Error:", error);
+}
+}
+elimina.addEventListener("click",deleteCard)
